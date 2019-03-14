43427a2fd8770ef9a5c3414771a41fff2278c800
Nick McGuire <nick@nickmcguire.com>
2019-03-13 22:32:43 -0400

    Markdown files render!
    Following the tutorial I now have pages and markdown files loading :tada:

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    siteMetadata: {
+    },
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`,
+            },
+        },
+        `gatsby-transformer-remark`,
+    ],
+};
